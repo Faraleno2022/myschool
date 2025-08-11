136a38d10ea7afc5d0c0550c46403b90c020f30e
Your Name <you@example.com>
2025-08-11 05:08:57 +0000

    Admin: enregistrement du modèle Ecole pour activer l'URL admin:eleves_ecole_add

--- eleves/admin.py
+++ eleves/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Ecole
 
-# Register your models here.
+
+@admin.register(Ecole)
+class EcoleAdmin(admin.ModelAdmin):
+    list_display = ("nom", "telephone", "email", "directeur")
+    search_fields = ("nom", "directeur", "telephone", "email")
